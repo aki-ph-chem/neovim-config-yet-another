09ac0b274748ee50fe0eef60e320126b9586741f
aki <lmxox@proton.me>
2026-06-23 03:15:06 +0900

    feat/lsp-more: add some key bindings for LSP

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -15,3 +15,33 @@ vim.opt.completeopt = { 'menuone', 'noselect', 'noinsert' }
 vim.o.autocomplete = true
 vim.o.autocompletedelay = 250
 vim.o.complete = '.,w,b,o'
+
+--- jump to definition by 'gd'
+vim.keymap.set('n', 'gd', function()
+  vim.lsp.buf.definition()
+end)
+
+-- hover
+vim.keymap.set('n', 'gh', function()
+  vim.lsp.buf.hover()
+end)
+
+-- show ref all
+vim.keymap.set('n', 'gr', function()
+  vim.lsp.buf.references()
+end)
+
+-- rename all
+vim.keymap.set('n', 'gn', function()
+  vim.lsp.buf.rename()
+end)
+
+-- list up all symobl in current buffer
+vim.keymap.set('n', 'gl', function()
+  vim.lsp.buf.document_symbol()
+end)
+
+-- list up all symobl in all buffers
+vim.keymap.set('n', 'ga', function()
+  vim.lsp.buf.workspace_symbol()
+end)
